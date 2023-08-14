7d12bc4624ab1894b082cf32bc6eb9448b2d252e
Svyntsov Andriy <svincov_a@ukr.net>
2023-08-14 18:06:13 +0300

    14.08

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,48 +1,47 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-const modalRoot = document.querySelector("#modal-root");
+const modalRoot = document.querySelector('#modal-root');
 
 class Modal extends Component {
-
-    componentDidMount() {
+  componentDidMount() {
     window.addEventListener('keydown', this.handleKeydownEscape);
-    };
+  }
 
-    componentWillUnmount() {
+  componentWillUnmount() {
     window.removeEventListener('keydown', this.handleKeydownEscape);
-    };
+  }
 
-    handleKeydownEscape = e => {
-        if (e.code === 'Escape') {
-            this.props.onChangeStatusModal();
-        };
-    };
-
-    handleOnClick = e => {
-        if (e.currentTarget === e.target) {
-            this.props.onChangeStatusModal();
-        }
-    };
-
-    render() {
-        return createPortal (
-            <div onClick={this.handleOnClick}
-                className={css.Overlay}>
-                <div className={css.Modal}>
-                    <img src={this.props.largeImg} alt={this.props.name} />
-                </div>
-            </div>, modalRoot);
+  handleKeydownEscape = e => {
+    if (e.code === 'Escape') {
+      this.props.onChangeStatusModal();
     }
-};
+  };
+
+  handleOnClick = e => {
+    if (e.currentTarget === e.target) {
+      this.props.onChangeStatusModal();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div onClick={this.handleOnClick} className={css.Overlay}>
+        <div className={css.Modal}>
+          <img src={this.props.largeImg} alt={this.props.name} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
 
 export default Modal;
 
-
 Modal.propTypes = {
-    onChangeStatusModal: PropTypes.func.isRequired,
-    name: PropTypes.string.isRequired,
-    largeImg: PropTypes.string.isRequired,
+  onChangeStatusModal: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  largeImg: PropTypes.string.isRequired,
 };
